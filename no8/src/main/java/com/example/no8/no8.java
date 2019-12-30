7a27a0d3aa8fc2730778ecf1b16e788a6b194eb9
3a513120 <57247605+3a513120@users.noreply.github.com>
2019-12-31 04:11:55 +0800

    Add files via upload

---
+++ no8/src/main/java/com/example/no8/no8.java
@@ -0,0 +1,10 @@
+package com.example.no8;
+
+import java.util.Scanner;
+
+public class no8 {
+    public static void main(String[] args) {
+        Scanner scanner =new Scanner(System.in);
+        CppBank cppBank = new CppBank(Integer.parseInt(scanner.next()),Integer.parseInt(scanner.next()));
+    }
+}
